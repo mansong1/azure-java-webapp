eefa943dc4e4aad6990cf3a6241048813758c295
Martin Ansong <martin.ansong@harness.io>
2024-02-09 09:49:25 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
   type        = string
+  default     = "rg_webapp"
 }
 
 variable "service_plan_name" {
   description = "Name of the Azure service plan"
-  type        = string  
+  type        = string
+  default     = "sp_java"
 }
 
 variable "web_app_name" {
   description = "Name of the Azure Linux web app"
   type        = string
+  default     = "helloworld"
 }
